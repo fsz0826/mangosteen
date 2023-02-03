da27269f682d54d243f48f2a2d5a0242c9e32ce2
fsz <513302151@qq.com>
2023-02-03 14:06:29 +0800

    修改默认路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,10 +7,12 @@ import Third from "../components/welcome/Third.vue";
 import Forth from "../components/welcome/Forth.vue";
 
 const routes: RouteRecordRaw[] = [
+  { path: "/", redirect: "/welcome" },
   {
     path: "/welcome",
     component: Welcome,
     children: [
+      { path: "", redirect: "/welcome/1" },
       { path: "1", component: First },
       { path: "2", component: Second },
       { path: "3", component: Third },
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts"></script>
 <template>
   <div class="wrapper">
-    <header>header</header>
+    <header>
+      <img class="logo" src="" alt="" />
+      <h1>飞老板飞老板</h1>
+    </header>
     <main><router-view /></main>
   </div>
 </template>
@@ -17,8 +20,16 @@
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    padding-top: 64px;
+    padding-top: 58px;
     color: #d4d4ee;
+
+    > .logo {
+      width: 100px;
+      height: 100px;
+    }
+    > h1 {
+      margin-top: 6px;
+    }
   }
   > main {
     flex-grow: 1;
