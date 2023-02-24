227b40d040536a8c5147b4d7727f19ee7061b763
fsz <513302151@qq.com>
2023-02-24 15:39:13 +0800

    创建FloatButton组件

--- src/components/shared/FloatBotton.vue
+++ src/components/shared/FloatBotton.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-    <SvgIcon name="add" />
+  <div class="wrapper">
+    <SvgIcon class="icon" name="add" />
   </div>
 </template>
 <script lang="ts" setup></script>
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.wrapper{
+  background: var(--float-button-bg);
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: 16px;
+  right: 16px;
+  .icon{
+    width: 24px;
+    height: 24px;
+    fill: var(--float-button-text);
+  }
+}
+
+</style>
