1d721934e9540b9fd8207258eda2e42a15b358a9
fsz <513302151@qq.com>
2023-03-01 15:06:18 +0800

    创建 MainLayout 组件并用其布局

---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Navbar>
+      <template #icon>
+        <slot name="icon"/>
+      </template>
+      <template #title>
+        <slot name="title"/>
+      </template>
+    </Navbar>
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Navbar from "../shared/Navbar.vue";
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -1,37 +1,36 @@
 <template>
-  <nav>
-    <Navbar>
-      <template #icon>
-        <SvgIcon name="menu" class="nav_icon" @click="onClickMenu"/>
-      </template>
-      <template #title> 山竹记账 </template>
-    </Navbar>
-  </nav>
-  <Center class="center_icon_wrapper">
-    <SvgIcon name="pig" class="center_icon" />
-  </Center>
-  <div class="button_wrapper">
+  <MainLayout>
+    <template #icon>
+      <SvgIcon name="menu" class="nav_icon" @click="onClickMenu" />
+    </template>
+    <template #title> 山竹记账 </template>
+    <Center class="center_icon_wrapper">
+      <SvgIcon name="pig" class="center_icon" />
+    </Center>
+    <div class="button_wrapper">
+      <router-link to="/items/create">
+        <Button class="button">开始记账</Button>
+      </router-link>
+    </div>
     <router-link to="/items/create">
-      <Button class="button">开始记账</Button>
+      <FloatButton icon-name="add" />
     </router-link>
-  </div>
-  <router-link to="/items/create">
-    <FloatButton icon-name="add" />
-  </router-link>
-  <Overlay v-if="refOverlayVisible" :on-close="onClickMenu"/>
+    <Overlay v-if="refOverlayVisible" :on-close="onClickMenu" />
+  </MainLayout>
 </template>
 <script lang="ts" setup>
 import Button from "../components/shared/Button.vue";
 import FloatButton from "../components/shared/FloatButton.vue";
 import Center from "../components/shared/Center.vue";
 import Navbar from "../components/shared/Navbar.vue";
-import Overlay from "../components/shared/Overlay.vue"
-import {ref} from "vue";
+import Overlay from "../components/shared/Overlay.vue";
+import { ref } from "vue";
+import MainLayout from "../components/layouts/MainLayout.vue";
 
-const refOverlayVisible = ref(false)
-const onClickMenu=()=>{
-  refOverlayVisible.value = !refOverlayVisible.value
-}
+const refOverlayVisible = ref(false);
+const onClickMenu = () => {
+  refOverlayVisible.value = !refOverlayVisible.value;
+};
 </script>
 <style lang="scss" scoped>
 .nav_icon {
