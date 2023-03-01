b9be48ebf79fbe2260d08138b5f6648ad300b05e
fsz <513302151@qq.com>
2023-03-01 15:22:03 +0800

    正在实现 /items/create 页面

--- src/components/item/ItemCreate.vue
+++ src/components/item/ItemCreate.vue
@@ -1,9 +1,19 @@
 <template>
-
+  <MainLayout>
+    <template #icon>
+      <SvgIcon name="back" class="nav_icon"/>
+    </template>
+    <template #title>
+      记一笔
+    </template>
+    <template #default>
+      <div>main</div>
+    </template>
+  </MainLayout>
 </template>
 
 <script setup lang="ts">
-
+import MainLayout from "../layouts/MainLayout.vue";
 </script>
 
 <style scoped lang="scss">
--- src/views/ItemPage.vue
+++ src/views/ItemPage.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>hi</div>
-  <router-view></router-view>
+  <router-view/>
 </template>
 
 <script lang="ts" setup>
@@ -8,5 +7,10 @@
 </script>
 
 <style scoped lang="scss">
-
+.nav_icon {
+  width: 30px;
+  height: 30px;
+  position: relative;
+  top: 1.5px;
+}
 </style>
