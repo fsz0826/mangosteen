35b65aafca52378fa0e39a988b00f17af68dccfb
fsz0826 <513302151@qq.com>
2023-03-23 17:43:20 +0800

    开始完善输入

--- src/components/item/InputPad.vue
+++ src/components/item/InputPad.vue
@@ -15,7 +15,7 @@
         </Popup>
       </span>
     </span>
-    <span class="amount">11223.056</span>
+    <span class="amount">{{ amount }}</span>
   </div>
   <div class="buttons">
     <button v-for="(button, index) in buttons" :key="index" @click="button.onClick">
@@ -45,19 +45,78 @@ const hideDatePicker = () => {
   refDatePickerVisible.value = false;
 };
 
-const amountText = () => {};
+const amount = ref("0");
+
+const amountText = (x: string) => {
+  amount.value = x;
+};
 const buttons = [
-  { text: "1", onClick: () => {} },
-  { text: "2", onClick: () => {} },
-  { text: "3", onClick: () => {} },
-  { text: "4", onClick: () => {} },
-  { text: "5", onClick: () => {} },
-  { text: "6", onClick: () => {} },
-  { text: "7", onClick: () => {} },
-  { text: "8", onClick: () => {} },
-  { text: "9", onClick: () => {} },
-  { text: ".", onClick: () => {} },
-  { text: "0", onClick: () => {} },
+  {
+    text: "1",
+    onClick: () => {
+      amountText("1");
+    },
+  },
+  {
+    text: "2",
+    onClick: () => {
+      amountText("2");
+    },
+  },
+  {
+    text: "3",
+    onClick: () => {
+      amountText("3");
+    },
+  },
+  {
+    text: "4",
+    onClick: () => {
+      amountText("4");
+    },
+  },
+  {
+    text: "5",
+    onClick: () => {
+      amountText("5");
+    },
+  },
+  {
+    text: "6",
+    onClick: () => {
+      amountText("6");
+    },
+  },
+  {
+    text: "7",
+    onClick: () => {
+      amountText("7");
+    },
+  },
+  {
+    text: "8",
+    onClick: () => {
+      amountText("8");
+    },
+  },
+  {
+    text: "9",
+    onClick: () => {
+      amountText("9");
+    },
+  },
+  {
+    text: ".",
+    onClick: () => {
+      amountText(".");
+    },
+  },
+  {
+    text: "0",
+    onClick: () => {
+      amountText("0");
+    },
+  },
   { text: "清空", onClick: () => {} },
   { text: "提交", onClick: () => {} },
 ];
